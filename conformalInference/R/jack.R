5680432f4704f054780676acb7395a62e1321bb1
paolo-vergo <paolo.vergottini@gmail.com>
2022-02-02 15:06:15 +0100

    Including Jackknife+ to conformal.pred.jack function

--- conformalInference/R/jack.R
+++ conformalInference/R/jack.R
@@ -1,4 +1,4 @@
-#' Jackknife conformal prediction intervals.
+#' Jackknife or Jackknife + conformal prediction intervals.
 #'
 #' Compute prediction intervals using a jackknife variant of conformal
 #'   inference.
@@ -52,7 +52,10 @@
 #' @param alpha Miscoverage level for the prediction intervals, i.e., intervals
 #'   with coverage 1-alpha are formed. Default for alpha is 0.1.
 #' @param verbose Should intermediate progress be printed out? Default is FALSE.
-#'
+#' @param plus Should the Jaccknife+ version of the algorithm be used?
+#' This provides valid (1-2alpha)% prediction intervals, but has a higher 
+#' computational load. Default is FALSE.
+#' 
 #' @return A list with the following components: pred, lo, up, fit. The first
 #'   three are matrices of dimension n0 x m, and the last is a matrix of
 #'   dimension n x m. Recall that n0 is the number of rows of x0, and m is the
@@ -73,16 +76,24 @@
 #'   The code is structured in this way so that we may defined a single pair of
 #'   functions train.fun and predict.fun, over a set of m = 5 tuning parameter
 #'   values, instead of calling the conformal function separately m = 5 times.
-#'
+#' @details This function is based on the package \code{\link{future.apply}} to
+#'  perform parallelization. If this package is not installed, then the computation
+#'  will proceed serially.
+#'   
 #' @seealso \code{\link{conformal.pred}}, 
 #'   \code{\link{conformal.pred.split}}, \code{\link{conformal.pred.roo}}
 #' @references "Distribution-Free Predictive Inference for Regression" by Lei,
-#'   G'Sell, Rinaldo, Tibshirani, Wasserman (2018).
+#'   G'Sell, Rinaldo, Tibshirani, Wasserman (2018). "Predictive Inference with the 
+#'   Jacknife+" by Barber,Candès, Ramdas, Tibshirani (2021).
+#'   
 #' @examples ## See examples for conformal.pred function
 #' @export conformal.pred.jack
+#' 
+#' 
 
 conformal.pred.jack = function(x, y, x0, train.fun, predict.fun, alpha=0.1, 
-  special.fun=NULL, mad.train.fun=NULL, mad.predict.fun=NULL, verbose=FALSE) {
+  special.fun=NULL, mad.train.fun=NULL, mad.predict.fun=NULL, 
+  verbose=FALSE, plus=FALSE) {
 
   # Set up data
   x = as.matrix(x)
@@ -91,11 +102,17 @@ conformal.pred.jack = function(x, y, x0, train.fun, predict.fun, alpha=0.1,
   p = ncol(x)
   x0 = matrix(x0,ncol=p)
   n0 = nrow(x0)
+  paral = "future.apply" %in% rownames(installed.packages())
   
   # Check input arguments
   check.args(x=x,y=y,x0=x0,alpha=alpha,train.fun=train.fun,
              predict.fun=predict.fun,mad.train.fun=mad.train.fun,
              mad.predict.fun=mad.predict.fun,special.fun=special.fun)
+  
+  #Check on plus argument
+  
+  if(!is.logical(plus))
+    stop("The argument 'plus' should be a logical \n")
   
   # Users may pass in a string for the verbose argument
   if (verbose == TRUE) txt = ""
@@ -103,6 +120,13 @@ conformal.pred.jack = function(x, y, x0, train.fun, predict.fun, alpha=0.1,
     txt = verbose
     verbose = TRUE
   }
+  
+  if(paral){
+    # Define structures for parallelisation
+    future::plan(future::multisession)
+    options(future.rng.onMisuse="ignore") 
+  }
+    
 
   if (verbose) cat(sprintf("%sInitial training on full data set ...\n",txt))
   
@@ -111,6 +135,14 @@ conformal.pred.jack = function(x, y, x0, train.fun, predict.fun, alpha=0.1,
   fit = matrix(predict.fun(out,x),nrow=n)
   pred = matrix(predict.fun(out,x0),nrow=n0)
   m = ncol(pred)
+  
+  # Compute models removing one observation each time
+  updated_models = one.lapply(1:n,function(jj){
+      mod_jj = train.fun(x[-jj,],y[-jj])
+      return(mod_jj)
+  })
+  
+  
 
   # Compute leave-one-out residuals with special function
   if (!is.null(special.fun) &&
@@ -120,47 +152,98 @@ conformal.pred.jack = function(x, y, x0, train.fun, predict.fun, alpha=0.1,
   
   # Compute leave-one-out residuals by brute-force
   else {
+    
     res = matrix(0,n,m)
-    for (i in 1:n) {
-      if (verbose) {
-        cat(sprintf("\r%sProcessing training point %i (of %i) ...",txt,i,n))
-        flush.console()
-      }
-     
-      xx = x[-i,]; yy = y[-i]
-      out.i = train.fun(xx,yy)
-      res[i,] = abs(y[i] - predict.fun(out.i,x[i,,drop=F]))
-
-      # Local scoring?
-      if (!is.null(mad.train.fun) && !is.null(mad.predict.fun)) {
-        r = abs(y[-i] - matrix(predict.fun(out.i,x[-i,,drop=F]),nrow=n-1))
-        for (l in 1:m) {
-          out.mad.i = mad.train.fun(x[-i,,drop=F],r[,l])
-          res[i,l] = res[i,l] / mad.predict.fun(out.mad.i,x[i,,drop=F])
+    
+      res=t(one.sapply(1:n,function(i){
+        
+        if (verbose) {
+          cat(sprintf("\r%sProcessing training point %i (of %i) ...",txt,i,n))
+          flush.console()
         }
-      }
-    }
-    if (verbose) cat("\n")
+        
+        test_val = matrix(x[i,],nrow=1)
+        pred_values = c(predict.fun(updated_models[[i]] ,  test_val ))
+        val=abs(y[i]- pred_values)
+        
+        # Local scoring?
+        if (!is.null(mad.train.fun) && !is.null(mad.predict.fun)) {
+          r = abs(y[-i] - matrix(predict.fun(updated_models[[i]],x[-i,,drop=F]),
+                                 nrow=n-1))
+          for (l in 1:m) {
+            out.mad.i = mad.train.fun(x[-i,,drop=F],r[,l])
+            val[i,l] = val[i,l] / mad.predict.fun(out.mad.i,x[i,,drop=F])
+          }
+        }
+        
+        if (verbose) cat("\n")
+        
+        return(val)
+      }))
+      
+
   }
+    
 
-  # Compute quantiles and intervals
-  lo = up = matrix(0,n0,m)
+    lo = up = matrix(0,n0,m)
+    
+  # Compute quantiles and intervals - JACKKNIFE +
+    if(plus){
 
-  for (l in 1:m) {
-    # Local scoring?
-    if (!is.null(mad.train.fun) && !is.null(mad.predict.fun)) {
-      res.train = abs(y - fit[,l])
-      mad.out = mad.train.fun(x,res.train)
-      mad.x0 = mad.predict.fun(mad.out,x0)
-    }
-    else {
-      mad.x0 = rep(1,n0)
+    
+    lo_up = t(one.sapply(1:n0, function(ii){
+      
+      #Compute new responses removing one observation from models at each time
+      mu=matrix(one.sapply(1:n, function(jj){
+        return(c(predict.fun(updated_models [[jj]],matrix(x0[ii,],nrow=1))))
+      }),nrow=n)
+      
+      # Get quantiles
+
+      lo_vec=mu-res
+      up_vec=mu+res
+      lo.obs <- one.sapply(1:m, function(l) quantile(lo_vec[,l],alpha))
+      up.obs <- one.sapply(1:m, function(l) quantile(up_vec[,l],1-alpha))
+      
+      return(cbind(lo.obs,up.obs))
+    }))
+    
+    #Obtain final lo and up bound
+    lo=lo_up[,1:m]
+    up=lo_up[,(m+1) : (2*m)]
+    
     }
     
-    q = quantile(res[,l],1-alpha)
-    lo[,l] = pred[,l] - q * mad.x0
-    up[,l] = pred[,l] + q * mad.x0
+    
+    # Compute quantiles and intervals - JACKKNIFE
+    else{
+      for (l in 1:m) {
+        # Local scoring?
+        if (!is.null(mad.train.fun) && !is.null(mad.predict.fun)) {
+          res.train = abs(y - fit[,l])
+          mad.out = mad.train.fun(x,res.train)
+          mad.x0 = mad.predict.fun(mad.out,x0)
+        }
+        else {
+          mad.x0 = rep(1,n0)
+        }
+    
+      q = quantile(res[,l],1-alpha)
+      lo[,l] = pred[,l] - q * mad.x0
+      up[,l] = pred[,l] + q * mad.x0
   }
+    }
+    
+    
+    if(paral){
+      ## To avoid CRAN check errors
+      ## R CMD check: make sure any open connections are closed afterward
+      future::plan(future::sequential)
+    }
+    
+    
   
   return(list(pred=pred,lo=lo,up=up,fit=fit))
 }
+
+
