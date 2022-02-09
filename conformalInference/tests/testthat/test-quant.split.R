921339cd02416dea8e9b6a903376d06b47c64383
paolo-vergo <paolo.vergottini@gmail.com>
2022-02-09 15:30:30 +0100

    Add testing with testthat

---
+++ conformalInference/tests/testthat/test-quant.split.R
@@ -0,0 +1,96 @@
+#set data
+
+set.seed(1234)
+n = 200; p = 100; s = 10
+x = matrix(rnorm(n*p),n,p)
+beta = c(rnorm(s),rep(0,p-s)) 
+y = x %*% beta + rnorm(n)
+n0 = 100
+x0 = matrix(rnorm(n0*p),n0,p)
+y0 = x0 %*% beta + rnorm(n0)
+funs=lasso.funs()
+
+
+#test
+
+test_that("Argument Errors", {
+  expect_error(conformal.quant.split(x, y, x0, method="dream", nthreads = 8,
+                                                alpha=0.1, 
+                                                gamma=0.1/2, split=NULL, 
+                                                seed=NULL, verbose=FALSE), "The 'method' argument is not correct. \n             Please select one of the following:classic, median, scaled.")
+  
+  expect_error(conformal.quant.split(x="ghtjbvhh", y, x0, method="classic", nthreads = 8,
+                                     alpha=0.1, 
+                                     gamma=0.1/2, split=NULL, 
+                                     seed=NULL, verbose=FALSE), "x must be a numeric matrix")
+  
+  
+  expect_error(conformal.quant.split(x, y, x0="yaaaaaa", method="classic", nthreads = 8,
+                                     alpha=0.1, 
+                                     gamma=0.1/2, split=NULL, 
+                                     seed=NULL, verbose=FALSE), "x0 must be a numeric matrix")
+  
+})
+
+test_that("Correct type", {
+  expect_type(conformal.quant.split(x, y, x0, method="classic", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$lo, "double")
+  expect_type(conformal.quant.split(x, y, x0, method="classic", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$up, "double")
+  
+  expect_type(conformal.quant.split(x, y, x0, method="median", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$lo, "double")
+  expect_type(conformal.quant.split(x, y, x0, method="median", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$up, "double")
+  
+  expect_type(conformal.quant.split(x, y, x0, method="scaled", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$lo, "double")
+  expect_type(conformal.quant.split(x, y, x0, method="scaled", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$up, "double")
+})
+
+test_that("Correct length", {
+  expect_length(conformal.quant.split(x, y, x0, method="classic", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$lo,n0)
+  expect_length(conformal.quant.split(x, y, x0, method="classic", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$up,n0)
+  
+  expect_length(conformal.quant.split(x, y, x0, method="median", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$lo,n0)
+  expect_length(conformal.quant.split(x, y, x0, method="median", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$up,n0)
+  
+  expect_length(conformal.quant.split(x, y, x0, method="scaled", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$lo,n0)
+  expect_length(conformal.quant.split(x, y, x0, method="scaled", nthreads = 8,
+                                    alpha=0.1, 
+                                    gamma=0.1/2, split=NULL, 
+                                    seed=NULL, verbose=FALSE)$up ,n0)
+})
+
+
+
+
+
