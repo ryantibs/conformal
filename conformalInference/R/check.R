8dee1eb024d78159e4bb898a38654c126e559965
ariane-cwi <77406785+ariane-cwi@users.noreply.github.com>
2023-09-21 18:29:35 +0200

    Update check.R

--- conformalInference/R/check.R
+++ conformalInference/R/check.R
@@ -40,7 +40,7 @@ check.args.surv = function(x=NULL, t=NULL, d=NULL, tau=NULL, x0=NULL, w=NULL,
     stop("cens.model must be km, rsf or cox")
   check.pos.num(tau)
   check.num.01(alpha)
-  check.args = function(x, t, x0, alpha, train.fun,
+  check.args(x, t, x0, alpha, train.fun,
     predict.fun, mad.train.fun, mad.predict.fun, special.fun)
 }
 
